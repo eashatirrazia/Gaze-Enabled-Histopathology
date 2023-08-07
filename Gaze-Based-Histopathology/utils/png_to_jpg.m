a84792fa5fa26149b62abe6ba8d6e8f8d62bc655
Easha Tir Razia <110398766+eashatirrazia@users.noreply.github.com>
2023-08-07 20:28:15 +0500

    my changes

--- Gaze-Based-Histopathology/utils/png_to_jpg.m
+++ Gaze-Based-Histopathology/utils/png_to_jpg.m
@@ -2,10 +2,10 @@ clc
 clear all
 close all
 
-filenames = dir('test\*.png');
+filenames = dir('C:\Users\DELL\Desktop\Gaze_Data\images\test\*.png');
 count = 1;
 for j=1:length(filenames)
-    img_png = imread(strcat('test\',filenames(j).name));
+    img_png = imread(strcat('C:\Users\DELL\Desktop\Gaze_Data\images\test\',filenames(j).name));
     [folder, fileName, extension] = fileparts(filenames(j).name);
     imwrite(img_png,strcat(fileName,'.jpg'),'Quality',100);
     count = count +1;
